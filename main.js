d82dc6b83bfb5dfec1976f6a1caa94f6256506e3
lisa <forazadan@mail-2-you.com>
2019-02-21 15:38:27 +0300

    Выполнено домашнее задание №3

--- main.js
+++ main.js
@@ -1,150 +1,71 @@
-//1. Почему код дает именно такие результаты?
-var a = 1,b = 1,c, d;
+// 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100
 
-c = ++a; alert(c); // 2, сначала применен префиксный инкремент к а(a=2), потом результат передан в с 
-d = b++; alert(d); // 1, сначала возвращается результат равный b в переменную d, потом постфиксный инкремент увеличивает b(b=2)
-c = (2 + ++a); alert(c); // 5, сначала применен преф. инкремент к а (а=3), затем выполнено сложение (2+a => 2+3) и результат передан в с
-d = (2 + b++); alert(d); // 4, сначала - сложение (2+b => 2+2), затем этот результат передается в d, после постфиксный инкремент увеличивает b (b=3)
-alert(a); // 3, т.к. переменная хранит перезаписанное в результате математических операций значений
-alert(b); // 3, аналогично,  т.к. переменная хранит перезаписанное в результате математических операций значений
-
-
-//2. Чему будет равен x?
-var a = 2; // в переменную а передаем значение 2
-var x = 1 + (a *= 2); // сначала выподняется действие в скобках (a*=2 => a=a*2 => a=4)
-                     //затем выполняется сложение (1+4),  итоговое значение записывается в x => x=5
-console.log(x);                   
-
-
-
-/*  3. Объявить две целочисленные переменные — a и b и задать им произвольные начальные значения. 
-Затем написать скрипт, который работает по следующему принципу:  
-    o если a и b положительные, вывести их разность;
-    o если а и b отрицательные, вывести их произведение;
-    o если а и b разных знаков, вывести их сумму;
-    Ноль можно считать положительным числом. */
-var a = 5;
-var b = 8;
-
-if (a >= 0 && b >= 0) {
-    console.log(a - b);
-} else if (a < 0 && b < 0) {
-    console.log(a * b);
-} else console.log(a + b);
-
-//4. Присвоить переменной а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от a до 15.
-var a = 7;
-switch (a) {
-    case 0:
-        console.log(0);
-    case 1:
-        console.log(1);
-    case 2:
-        console.log(2);
-    case 3:
-        console.log(3);
-    case 4:
-        console.log(4);
-    case 5:
-        console.log(5);
-    case 6:
-        console.log(6);
-    case 7:
-        console.log(7);
-    case 8:
-        console.log(8);
-    case 9:
-        console.log(9);
-    case 10:
-        console.log(10);
-    case 11:
-        console.log(11);
-    case 12:
-        console.log(12);
-    case 13:
-        console.log(13);
-    case 14:
-        console.log(14);
-    case 15:
-        console.log(15);
-        break;
-}
-
-//5. Реализовать четыре основные арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
-function sum(a, b) {
-    return a + b;
-}
-
-function difference(a, b) {
-    return a - b;
-}
-
-function product(a, b) {
-    return a * b;
-}
-
-function quotient(a, b) {
-    return b == 0 ? "на ноль делить нельзя" : a / b;
-}
-
-console.log(sum(3, 5));
-console.log(difference(2, 4));
-console.log(product(3, 1));
-console.log(quotient(2, 4));
-
-/* 6. Реализовать функцию с тремя параметрами: function mathOperation(arg1, arg2, operation),
-где arg1, arg2 — значения аргументов, operation — строка с названием операции. 
-В зависимости от переданного значения выполнить одну из арифметических операций
-(использовать функции из пункта 5) и вернуть полученное значение (применить switch). */
-
-/* Я так понимаю, что нужно показать суть понимания переиспользования функций. 
-Поэтому прошу считать функции sum, difference уже определенными выше.  */
-
-function mathOperation(arg1, arg2, operation) {
-    switch (operation) {
-        case sum:
-            return sum(arg1, arg2);
-            break;
-        case difference:
-            return difference(arg1, arg2);
-            break;
-        case product:
-            return product(arg1, arg2);
-            break;
-        case quotient:
-            return quotient(arg1, arg2);
-            break;
+// простоe число - делится только на еденицу и на само себя
+//поэтому начинаем проверку c двоек
+var i = 2;
+isPrimeNumber:
+    while (i < 100) {
+        var j = 2;
+        //смотрим, больше ли делимое делителя
+        while (i > j) {
+            //делится ли число на любой другое больше двух без остатка
+            //если делится - увеличивем i и снова заходим в внешний цикл while
+            if (i % j == 0) {
+                i++;
+                continue isPrimeNumber;
+            }
+            //не делится -увеличивем делитель
+            else j++;
+        }
+        console.log(i)
+        i++;
     }
+
+
+
+
+//2.Товары в корзине хранятся в массиве. Задачи:
+//  a.Организовать такой массив для хранения товаров в корзине;
+//  b.Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+
+var product1 = {
+    price: 50
+};
+
+var product2 = {
+    price: 60
+};
+
+var product3 = {
+    price: 70
+};
+
+var basket = [product1, product2, product3];
+var summ = 0;
+
+function countBasketPrice(basket) {
+    for (var i = 0; i < basket.length; i++) {
+        summ += basket[i].price
+    }
+    return summ
 }
 
-console.log(mathOperation(2, 1, sum));
-console.log(mathOperation(-5, 3, difference));
-console.log(mathOperation(-2, 2, product));
-console.log(mathOperation(4, 0, quotient));
+console.log(countBasketPrice(basket));
 
-// 7.Сравнить null и 0. Объяснить результат.
-null == 0;  // false, 
-null !=0;   //true
-null === 0; // false,
-null < 0;   // false,
-null > 0;   // false,
-null >= 0;  // true,
-null <= 0;  // true,
+// 3.Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:for(…){// здесь пусто}
 
-/* Проверка на нестрогое и строгое равенство (неравенство): null равен только undefined.
-Проверка на <,>,<=,>= происходит с приведением типов. Null преобразовывется в 0.
-*/
+for (var i = 0; i < 11; console.log(i++)) {}
 
-/* 8. С помощью рекурсии организовать функцию возведения числа в степень. 
-Формат: function power(val, pow), где val — заданное число, pow –— степень. */
-function power(val, pow) {
-    if (pow > 1) {
-      return val * power(val, pow - 1);
-    } else {
-      return val;
-    }
-  }
-  
-console.log(power(2, 4));
+// 4.Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
 
-
+var starElemet = "x";
+var starArray = [];
+for (var i = 0; i < 20; i++) {
+    starArray.push(starElemet);
+    console.log(starArray.join(" "));
+}
